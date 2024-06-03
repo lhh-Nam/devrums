dde6d22233be186ec487aa050703c455106a66b2
Nam Le <lehoanghuynam0401@gmail.com>
2024-06-03 23:01:03 +0700

    setup nextjs

---
+++ client/src/app/(main-layout)/home/[slug]/page.tsx
@@ -0,0 +1,3 @@
+export default function HomeDetail() {
+  return <main>HomeDetail</main>;
+}
---
+++ client/src/app/auth/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Devrums",
+  description: "Rooms for Developer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div>This is Auth Layout</div>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+}
